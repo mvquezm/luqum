e67299797b74444aad05b5e315130187108a1d2e
Michel Velázquez Mariño <michel.velazquez@linos.es>
2018-08-10 09:33:40 +0200

    [jluqum] minor improvements
      - ignored some files
      - add more queries to test case

--- jluqum/Jluqum/src/test/java/linos/jluqum/parsers/LuceneCompilerTest.java
+++ jluqum/Jluqum/src/test/java/linos/jluqum/parsers/LuceneCompilerTest.java
@@ -3,17 +3,23 @@ package linos.jluqum.parsers;
 import org.antlr.runtime.tree.CommonTree;
 import org.junit.Test;
 
-/**
- * Created by jorge.hernandez on 09/08/2018.
- */
+import java.util.Arrays;
+import java.util.List;
+
 public class LuceneCompilerTest {
 
     @Test
     public void testCorrectExpression() {
         //compile the expression
         LuceneCompiler compiler = new LuceneCompiler();
-        CommonTree ast = compiler.compile("field1:(+term1 -term2) OR ( (+term3 -term4) NOT field1:(\"phrase one\" +term3))");
+        List<String> queries = Arrays.asList(
+                "field1:(+term1 -term2) OR ( (+term3 -term4) NOT field1:(\"phrase one\" +term3))",
+                "field1:(+term1 -term2) OR ( (term3 OR term4) NOT field1:(\"phrase one\" OR +term3) )"
+        );
 
-
+        for (String query: queries) {
+            System.out.println(String.format("Query: %s", query));
+            CommonTree ast = compiler.compile(query);
+        }
     }
 }
